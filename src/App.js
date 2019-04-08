386013b0eec187564164f3805c134e670fd1184e
Josh Leong <leong.jo@gmail.com>
2019-04-07 22:14:59 -0600

    added css import

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button color="danger">Danger!</Button>
+      <h1>COCreport.io</h1>
+        <Button color="danger">Send Report</Button>
       </div>
     );
   }
